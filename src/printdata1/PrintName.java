cc23c31d8565397d9c552c66245c53629bbf7e09
Divya <divyapatel.dp908@gmail.com>
2022-07-14 18:16:58 -0400

    data test

---
+++ src/printdata1/PrintName.java
@@ -0,0 +1,20 @@
+package printdata;
+
+public class PrintName {
+
+    public static void main(String []args)
+    {
+        String str = "Divya\n";
+        //System.out.println(str.repeat (count: 10));
+        //String str = "Divya\n";
+        //System.out.println(str.replace(10));
+        //System.out.println(str.);
+        //int i = 0;
+        //while (i < 10) {
+          //  System.out.println("Divya");
+            //i++;
+        //System.out.println(str.repeat(count));
+        }
+        
+    }
+
---
+++ src/printdata1/PrintNames.java
@@ -0,0 +1,9 @@
+package printdata;
+
+public class PrintNames {
+    public static void main(String args[])
+    {
+        String str = "Divta";
+        //System.out.println(str.);
+    }
+}
